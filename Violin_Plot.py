3da06ee27080a2fda2f8e737350cd35a63da966c
Ahmed-Bilal-27 <ahmedbilal3830@gmail.com>
2023-12-27 11:15:58 +0500

    Sixth Commit dated December 27, 2023

---
+++ Violin_Plot.py
@@ -0,0 +1,28 @@
+"""
+This code is part of my Artificial Intelligence (CSC-462) lab coursework.
+"""
+
+#----------------Imports-----------------
+import pandas as pd # URL: https://pandas.pydata.org/
+import seaborn as sns # URL: https://seaborn.pydata.org/
+import matplotlib.pyplot as plt # URL: https://matplotlib.org/
+import numpy as np
+#----------------Imports-----------------
+# Reading dataset into pandas dataframe
+iris = pd.read_csv("D:\\Programs\\Python\\Exploratory_Data_Analysis\\iris.csv")
+# Displaying dataset characteristics
+print(iris.shape)
+print(iris.describe())
+print(iris.info())
+print(iris.head())
+print(iris.columns)
+# Checking whether the dataset is balanced or imbalanced
+print(iris['species'].value_counts())
+# Creating Violin Plots
+"""
+data=iris - It is the data to create plot of.
+x='species' - iris['species'] is the x-axis.
+y='petal_length' - iris['petal_length'] is the y-axis.
+"""
+sns.violinplot(data=iris, x='species', y='petal_length', hue=iris['species'])
+plt.show() # Displaying the plot
